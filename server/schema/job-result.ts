b82c08c4b98e38e6193f06c10d9f5309f940bb77
sirily11 <sirily1997@gmail.com>
2021-09-29 00:53:28 +0800

    feat: Update a message queue

---
+++ server/schema/job-result.ts
@@ -0,0 +1,34 @@
+/**
+ * Create a user object for mongoose ORM.
+ *
+ * This file contains the device schema for mongodb device collection
+ */
+import { Schema, model, connect, Document, Model } from "mongoose";
+import { Web3DataInfo } from "../client/node_data";
+import mongoose from "mongoose";
+
+export interface IJobResult extends Document {
+  time: Date;
+  deviceID: string;
+  /**
+   * From which client. This will be the unique id
+   */
+  from: string;
+  command: any;
+  result: any;
+}
+
+export const jobResultSchema = new Schema<IJobResult>({
+  time: { type: Date, required: true },
+  deviceID: { type: String, required: true },
+  from: { type: String, required: true },
+  command: { type: Schema.Types.Mixed, required: true },
+  result: { type: Schema.Types.Mixed, required: true },
+});
+
+/**
+ *
+ */
+export const JobResultModel: Model<IJobResult> = mongoose.models.job_result
+  ? mongoose.models.device
+  : model<IJobResult>("job_result", jobResultSchema);
