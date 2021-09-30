2cbe5650007cd902f0fd8ef9f2157d3279cae8ce
sirily11 <sirily1997@gmail.com>
2021-09-30 16:10:30 +0800

    fix: clean up

--- server/app.ts
+++ server/app.ts
@@ -1,17 +1,16 @@
 import next from "next";
 import express from "express";
-import { Server } from "./server";
+import {Server} from "./server";
 import mongoose from "mongoose";
 import Logger from "./logger";
-import { ClientPlugin } from "./plugin/plugins/socketIOPlugins/clientPlugin";
-import { createServer } from "http";
-import { AppPlugin } from "./plugin/plugins/socketIOPlugins/appPlugin";
-import { BaseSocketIOPlugin } from "./plugin/basePlugin";
-import { DBChangePlugin } from "./plugin/plugins/socketIOPlugins/dbPlugin";
+import {ClientPlugin} from "./plugin/plugins/socketIOPlugins/clientPlugin";
+import {createServer} from "http";
+import {AppPlugin} from "./plugin/plugins/socketIOPlugins/appPlugin";
+import {DBChangePlugin} from "./plugin/plugins/socketIOPlugins/dbPlugin";
 
 const port = parseInt(process.env.PORT!, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
-const nextApp = next({ dev });
+const nextApp = next({dev});
 const nextHandler = nextApp.getRequestHandler();
 
 nextApp.prepare().then(async () => {
