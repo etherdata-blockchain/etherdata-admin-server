64c5cceb00bb12d51e3a8706b2aeb184b7977131
sirily11 <sirily1997@gmail.com>
2021-10-04 15:57:56 +0800

    feat: add health checking api

---
+++ pages/api/v1/health/index.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { DeviceRegistrationPlugin } from "../../../../server/plugin/plugins/deviceRegistrationPlugin";
+import { JwtVerificationHandler } from "../../../../utils/nextHandler/jwtVerificationHandler";
+import { IDevice } from "../../../../server/schema/device";
+
+type Data = {
+    err?: string
+};
+
+/**
+ * Health checking
+ * @param req
+ * @param res
+ */
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200)
+}
+
+export default handler;
