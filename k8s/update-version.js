900b405554979173362259b16fd77ff7933102e8
sirily11 <sirily1997@gmail.com>
2021-11-18 20:49:42 +0800

    update version

--- k8s/update-version.js
+++ k8s/update-version.js
@@ -1,12 +1,15 @@
 const YAML = require("yaml");
-const fs = require("fs");
 
-const packageJsonFile = fs.readFileSync("./package.json", "utf-8");
-const deploymentFile = fs.readFileSync("./k8s/deployment.yml", "utf-8");
-const deploymentConfig = YAML.parse(deploymentFile);
-const packageJson = JSON.parse(packageJsonFile);
-const version = packageJson.version;
+module.exports.readVersion = function (contents) {
+  const yamlFileContent = YAML.parse(contents);
+  const image = yamlFileContent.spec.template.spec.containers[0].image;
+  const version = image.split(":")[1];
+  console.log(version);
+  return version;
+};
 
-console.log("Updating deployment version to: " + version);
-deploymentConfig.spec.template.spec.containers[0].image = `sirily11/etd-remote-admin-server:${version}`;
-fs.writeFileSync("./k8s/deployment.yml", YAML.stringify(deploymentConfig));
+module.exports.writeVersion = function (contents, version) {
+  const yamlFileContent = YAML.parse(contents);
+  yamlFileContent.spec.template.spec.containers[0].image = `sirily11/etd-remote-admin-server:${version}`;
+  return YAML.stringify(yamlFileContent);
+};
