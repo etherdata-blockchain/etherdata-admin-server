310ca2d6336aaa630022d6d5d993e905c9a426af
sirily11 <32106111+sirily11@users.noreply.github.com>
2021-11-18 14:44:57 +0000

    Deploying to k8s-release-dev from @ etherdata-blockchain/etherdata-admin-server@ef8108825632cb8064578a502dd2323b50656a34 🚀

---
+++ update-version.js
@@ -0,0 +1,14 @@
+const YAML = require("yaml");
+
+module.exports.readVersion = function (contents) {
+  const yamlFileContent = YAML.parse(contents);
+  const image = yamlFileContent.spec.template.spec.containers[0].image;
+  const version = image.split(":")[1];
+  return version.replace("v", "");
+};
+
+module.exports.writeVersion = function (contents, version) {
+  const yamlFileContent = YAML.parse(contents);
+  yamlFileContent.spec.template.spec.containers[0].image = `sirily11/etd-remote-admin-server:v${version}`;
+  return YAML.stringify(yamlFileContent);
+};
