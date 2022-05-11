ac0e2fb440005fb15f36bc16defb1a08d33437f0
sirily11 <32106111+sirily11@users.noreply.github.com>
2022-05-11 22:16:09 +0800

    fix: cannot search with special characters (#127)
    * fix: run execution plan error
    
    * fix: cannot search with special characters
    
    * also fixed cannot run execution plan with empty target device ids or group ids

--- tests/api/update-template/run.test.ts
+++ tests/api/update-template/run.test.ts
@@ -51,6 +51,8 @@ describe("Given a update script api handler", () => {
 
   afterEach(async () => {
     await schema.UpdateScriptModel.deleteMany({});
+    await schema.StorageOwnerModel.deleteMany({});
+    await schema.StorageItemModel.deleteMany({});
     await schema.PendingJobModel.deleteMany({});
   });
 
@@ -128,5 +130,45 @@ describe("Given a update script api handler", () => {
       "mock_ids",
       "mock_ids_2",
     ]);
+  });
+
+  test("When calling post without group ids", async () => {
+    await schema.StorageOwnerModel.create(mockData.MockUser);
+    await schema.StorageItemModel.create(mockData.MockStorageItem);
+    await schema.StorageItemModel.create(mockData.MockStorageItem2);
+
+    const executionPlanService = new dbServices.ExecutionPlanService();
+    const updateScriptData = await schema.UpdateScriptModel.create(
+      mockUpdateScriptData
+    );
+
+    const { req, res } = createMocks({
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+      query: {
+        id: updateScriptData._id.toString(),
+      },
+      body: {
+        targetGroupIds: [mockData.MockUser.user_id],
+      },
+    });
+
+    await handler(req as any, res as any);
+    expect(res.statusCode).toBe(StatusCodes.OK);
+
+    await utils.sleep(200);
+    const plans = (await executionPlanService.getPlans(updateScriptData._id))!;
+    expect(plans?.length).toBe(2);
+    expect(plans[0].isDone).toBeTruthy();
+    expect(plans[1].isDone).toBeTruthy();
+    expect(await schema.PendingJobModel.countDocuments({})).toBe(2);
+
+    const updatedTemplate = await schema.UpdateScriptModel.findOne({
+      _id: updateScriptData._id,
+    });
+    expect(updatedTemplate!.targetGroupIds).toHaveLength(1);
+    expect(updatedTemplate!.targetDeviceIds).toHaveLength(0);
   });
 });
