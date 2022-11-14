8e74fc27aab223bfa136c96bf24978403a7cd38a
sirily11 <32106111+sirily11@users.noreply.github.com>
2022-11-15 01:59:20 +0800

    fix: v2 api errors (#139)

---
+++ pages/api/v2/search.ts
@@ -0,0 +1,38 @@
+import {
+  methodAllowedHandler,
+  jwtVerificationHandler,
+} from "@etherdata-blockchain/next-js-handlers";
+import { dbServices } from "@etherdata-blockchain/services";
+import HTTPMethod from "http-method-enum";
+import { StatusCodes } from "http-status-codes";
+import { NextApiRequest, NextApiResponse } from "next";
+
+/**
+ * Search devices by keyword
+ * @param req
+ * @param res
+ * @returns
+ */
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { key } = req.query;
+
+  if (key?.length === 0) {
+    res.status(StatusCodes.OK).json([]);
+    return;
+  }
+
+  const storageManagementService = new dbServices.StorageManagementService();
+  const devices = await storageManagementService.search(key as string);
+
+  if (devices) {
+    res.status(StatusCodes.OK).json(devices);
+    return;
+  } else {
+    res.status(StatusCodes.OK).json([]);
+    return;
+  }
+}
+
+export default methodAllowedHandler(jwtVerificationHandler(handler as any), [
+  HTTPMethod.GET,
+]);
